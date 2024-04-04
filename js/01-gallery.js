0b1d05ba3a29d2451cca425cca8a9a4fda65a857
Tavsanka <domnikan@gmail.com>
2024-04-04 17:39:22 +0200

    updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from "./gallery-items.js";
+import "simplelightbox/dist/simple-lightbox.css";
 // Change code below this line
+const gallery = document.querySelector("ul.gallery");
+for (const item of galleryItems) {
+  const html = `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+  gallery.insertAdjacentHTML("beforeend", html);
+}
+
+const lightbox = new SimpleLightbox(".gallery a");
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+for (const item of galleryItems) {
+  const html = `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"/></a> </li>`;
+  gallery.insertAdjacentHTML("beforeend", html);
+}
 
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
 console.log(galleryItems);
